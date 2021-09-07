4b34be3d41e7dd32ad729d59b479bfcc85d18ffe
savanna chiang <savannaac@gmail.com>
2021-09-06 23:57:36 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -7,6 +7,15 @@ function changeState(state, action){
   }
 }
 
+function render() {
+  document.body.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
